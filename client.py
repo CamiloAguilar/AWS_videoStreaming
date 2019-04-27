43ec6b829e62799a306107a3b444f407bbffdc76
CamiloAguilar <aguilarcamiloa@gmail.com>
2019-04-27 10:40:36 -0500

    cambio de planes; otra prueba mas

--- client.py
+++ client.py
@@ -1,21 +1,22 @@
+import base64
 import cv2
-import numpy as np
-import socket
-import sys
-import pickle
-import struct
+import zmq
 
-cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('18.231.178.147', 8089)) ##localhost
+context = zmq.Context()
+footage_socket = context.socket(zmq.PUB)
+footage_socket.connect('tcp://18.231.178.147:8089')
+
+camera = cv2.VideoCapture(0)  # init the camera
 
 while True:
+    try:
+        grabbed, frame = camera.read()  # grab the current frame
+        frame = cv2.resize(frame, (640, 480))  # resize the frame
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_text = base64.b64encode(buffer)
+        footage_socket.send(jpg_as_text)
 
-    # Serialize frame
-    data = pickle.dumps(frame)
-
-    # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
-
-    # Then data
-    client_sock.sendall(message_size + data)
+    except KeyboardInterrupt:
+        camera.release()
+        cv2.destroyAllWindows()
+        break
