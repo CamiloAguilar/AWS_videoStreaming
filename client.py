cf6d24eeb1d814e49fbf63c8d1dbb80164ac159f
Camilo Aguilar <aguilarcamiloa@gmail.com>
2019-05-22 14:49:40 -0500

    add AWS IoT connection to script

--- client.py
+++ client.py
@@ -1,10 +1,49 @@
-import base64
+#import base64
 import cv2
-import zmq
+#import zmq
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient # Import the AWS IoT Device SDK
+import json # Import json for parson endpoint.json file
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.PUB)
-footage_socket.connect('tcp://18.231.178.147:8089')
+# Load the endpoint from file
+with open('./AWS_IoT/endpoint.json') as json_file:  
+    data = json.load(json_file)
+
+# temporalmente nos conectamo al dispositivo 'car1'
+deviceName = 'car1'
+
+# Build useful variable for code
+#subTopic = 'edx/messaging/' + deviceName
+pubTopic = 'edx/messaging/' + deviceName #destinationDeviceName
+keyPath = './AWS_IoT/private.pem.key'
+certPath = './AWS_IoT/certificate.pem.crt'
+caPath = './AWS_IoT/root-CA.crt'
+clientId = deviceName
+host = data['endpointAddress']
+port = 8883
+
+#********************************************************
+## Nos conectamos a AWS IoT usando las credenciales
+#********************************************************
+# Create an AWS IoT MQTT Client using TLSv1.2 Mutual Authentication
+myAWSIoTMQTTClient = AWSIoTMQTTClient(deviceName)
+
+# configureEndpoint
+myAWSIoTMQTTClient.configureEndpoint(host, port)
+
+# configureCredentials
+myAWSIoTMQTTClient.configureCredentials(caPath, keyPath, certPath)
+
+# Conectese al servicio AWS IoT mediante la funcion de conexion
+myAWSIoTMQTTClient.connect()
+
+# Function to publish payload to IoT topic
+def publishToIoTTopic(topic, payload):
+    # TODO 4: Implement function to publish to specified IoT topic using device object
+    #         that you will create
+    myAWSIoTMQTTClient.publish(topic, payload, 1)
+
+
+
 
 camera = cv2.VideoCapture(0)  # init the camera
 
@@ -13,8 +52,10 @@ while True:
         grabbed, frame = camera.read()  # grab the current frame
         frame = cv2.resize(frame, (640, 480))  # resize the frame
         encoded, buffer = cv2.imencode('.jpg', frame)
-        jpg_as_text = base64.b64encode(buffer)
-        footage_socket.send(jpg_as_text)
+
+        # Calling function to publish to IoT Topic
+        print('...enviando informacion a AWS')
+        publishToIoTTopic(pubTopic, 'gonocuca')
 
         key = cv2.waitKey(1) & 0xFF
         # if the `q` key was pressed, break from the loop
